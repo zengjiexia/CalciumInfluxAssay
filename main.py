d97e4c547a1b58957b10b600ad48a1d883cf18cd
Z Xia <zengjie.xia7@gmail.com>
2019-12-09 16:03:34 +0100

    Fix

--- main.py
+++ main.py
@@ -1,66 +1,84 @@
-
-
 from local_tools import *
 import os
 
+Path = r'/Users/zengjiexia/Desktop/2019-12-06 liposome_frac_2_Derya'
+
+
+result_path = r'/Users/zengjiexia/Desktop/results'
+
 holder = {
-'PATH' : r"D:\Temporary_20191205\20191205_Derya\1",
-'Threshold' : '80',
+'Threshold' : '50',
 'Radius' : '3',
 'High' : 200,
 'Low' : -100
 }
 
-sample_path = holder['PATH']
-path = {
-	'main': sample_path,
-	'ionomycin': sample_path + '/Ionomycin/',
-	'sample': sample_path + '/Sample/',
-	'blank': sample_path + '/Blank/'
-}
-# Error = 0, Safe = 1
-# error_report{'path': [main, ionomycin, sample, blank]}
-error_report = {'path': [os.path.isdir(p) for p in path.values()]} 
+def one_sample(root, item):
+	sample_path = os.path.join(root, item)
+	print('starting '+ sample_path)
+	path = {
+		'main': sample_path,
+		'ionomycin': sample_path + '/Ionomycin/',
+		'sample': sample_path + '/Sample/',
+		'blank': sample_path + '/Blank/'
+	}
+	# Error = 0, Safe = 1
+	# error_report{'path': [main, ionomycin, sample, blank]}
+	error_report = {'path': [os.path.isdir(p) for p in path.values()]} 
 
 
-ionomycin = []
-sample = []
-blank = []
+	ionomycin = []
+	sample = []
+	blank = []
 
-filenames = extract_filename(path['ionomycin'])
-for name in filenames:
+	filenames = extract_filename(path['ionomycin'])
+	for name in filenames:
 
-	ionomycin_mean = average_frame(path['ionomycin'] + name)
-	sample_mean = average_frame(path['sample'] + name)
-	blank_mean = average_frame(path['blank'] + name)
+		ionomycin_mean = average_frame(path['ionomycin'] + name)
+		sample_mean = average_frame(path['sample'] + name)
+		blank_mean = average_frame(path['blank'] + name)
 
-	sample_aligned, blank_aligned = img_alignment(ionomycin_mean, sample_mean, blank_mean)
+		sample_aligned, blank_aligned = img_alignment(ionomycin_mean, sample_mean, blank_mean)
 
-	ionomycin.append(ionomycin_mean)
-	sample.append(sample_aligned)
-	blank.append(blank_aligned)
+		ionomycin.append(ionomycin_mean)
+		sample.append(sample_aligned)
+		blank.append(blank_aligned)
+
+	Results = pd.DataFrame()
+
+	for n in range(0, len(ionomycin)):
+		print('working on file number '+str(n))
+
+		coordinations = peak_locating(ionomycin[n], 50)
+		peak = pd.DataFrame({
+			'field': np.repeat(n, len(coordinations)),
+			'x': coordinations[:, 0],
+			'y': coordinations[:, 1]
+			})
+
+		result, error = influx_calculation(
+			ionomycin[n],
+			sample[n],
+			blank[n],
+			peak,
+			holder['High'],
+			holder['Low'],
+			int(holder['Radius']))
+		error_report['calculation field ' + str(n)] = error
+
+		Results = pd.concat([Results, result])
+
+	Results.to_excel(result_path+'/'+ item+'.xlsx')
+	print('Done with '+ sample_path)
+
+	return Results.influx.mean()
 
 
 
-for n in range(0, len(ionomycin)):
-	coordinations = peak_locating(ionomycin[n], 80)
-	peak = pd.DataFrame({
-		'field': np.repeat(n, len(coordinations)),
-		'x': coordinations[:, 0],
-		'y': coordinations[:, 1]
-		})
-
-	result, error = influx_calculation(
-		ionomycin[n],
-		sample[n],
-		blank[n],
-		peak,
-		holder['High'],
-		holder['Low'],
-		int(holder['Radius']))
-	error_report['calculation field ' + str(n)] = error
-
-	print(result)
-
+samplenames = os.listdir(Path)
+for item in samplenames:
+	influx = one_sample(Path, item)
+	with open(r"/Users/zengjiexia/Desktop/results/summary.txt",'a') as f:
+		f.write(item + '\t' + str(influx)+'\n')
 
 
